b2dbd43e80362dcfa8bc7595923cd15f438299cf
SizableShrimp <29855886+SizableShrimp@users.noreply.github.com>
2020-12-09 15:45:17 -0600

    Update Day 9
    Convert List to array and other optimizations

--- src/main/java/me/sizableshrimp/adventofcode2020/days/Day09.java
+++ src/main/java/me/sizableshrimp/adventofcode2020/days/Day09.java
@@ -6,30 +6,29 @@
 package me.sizableshrimp.adventofcode2020.days;
 
 import me.sizableshrimp.adventofcode2020.helper.ArrayConvert;
-import me.sizableshrimp.adventofcode2020.helper.ListConvert;
-import me.sizableshrimp.adventofcode2020.helper.Streams;
 import me.sizableshrimp.adventofcode2020.templates.SeparatedDay;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.LongSummaryStatistics;
+import java.util.stream.LongStream;
 
 public class Day09 extends SeparatedDay {
-    private List<Long> all;
+    private long[] data;
     private int part1;
 
     @Override
     protected Object part1() {
         int windowSize = 25;
-        for (int i = windowSize; i < all.size(); i++) {
-            long input = all.get(i);
-            List<Long> window = all.subList(i - windowSize, i);
+        for (int i = windowSize; i < data.length; i++) {
+            long input = data[i];
+            int start = i - windowSize;
 
             boolean found = false;
-            for (long l : window) {
-                if (window.contains(input - l)) {
-                    found = true;
+            for (int j = start; j < i; j++) {
+                long num = data[j];
+                found = contains(data, start, i, input - num);
+                if (found)
                     break;
-                }
             }
 
             if (!found) {
@@ -45,13 +44,11 @@ public class Day09 extends SeparatedDay {
     protected Object part2() {
         if (part1 == 0)
             return null;
-        long target = all.get(part1);
-        // long[] prefixSums = ArrayConvert.prefixSumsLong(all);
+        long target = data[part1];
 
-        // System.out.println(part1);
         int left = part1 - 1;
         int right = part1;
-        long sum = all.get(left) + all.get(right);
+        long sum = data[left] + data[right];
 
         while (true) {
             if (sum < target) {
@@ -60,24 +57,33 @@ public class Day09 extends SeparatedDay {
 
                 // System.out.println("too low: " + left + " - " + right);
                 left--;
-                sum += all.get(left);
-            }  else if (sum > target) {
+                sum += data[left];
+            } else if (sum > target) {
                 // If the current sum is more than the target, we have too many numbers
                 // and need to slide our right bound to the left to get a lower sum.
 
                 // System.out.println("too high: " + left + " - " + right);
                 right--;
-                sum -= all.get(right + 1);
+                sum -= data[right + 1];
             } else {
-                List<Long> window = all.subList(left, right);
-                LongSummaryStatistics stats = Streams.unboxLongs(window).summaryStatistics();
+                long[] window = Arrays.copyOfRange(data, left, right);
+                LongSummaryStatistics stats = LongStream.of(window).summaryStatistics();
                 return stats.getMin() + stats.getMax();
             }
         }
     }
 
+    private boolean contains(long[] data, int start, int end, long find) {
+        for (int i = start; i < end; i++) {
+            if (data[i] == find)
+                return true;
+        }
+
+        return false;
+    }
+
     @Override
     protected void parse() {
-        all = ListConvert.longs(lines);
+        data = ArrayConvert.longs(lines);
     }
 }
